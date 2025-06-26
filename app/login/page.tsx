b180443677c1b6c709f25fa8e22ddf4c6e8fd429
SOUNAK GHOSH <sounak2309@gmail.com>
2025-06-26 09:40:37 +0530

    added pages

---
+++ app/login/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const router = useRouter();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const result = await signIn("credentials", {
+            email,
+            password,
+            redirect: false,
+        });
+
+        if (result?.error) {
+            console.log(result.error);
+        } else {
+            router.push("/");
+        }
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    placeholder="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+            <div>
+                <p>
+                    Don&apos;t have an account?{" "}
+                    <a href="/register">Register</a>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
