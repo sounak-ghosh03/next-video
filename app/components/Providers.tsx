7d022951b28ad8b8ff1b12f401309521d586825b
SOUNAK GHOSH <sounak2309@gmail.com>
2025-06-26 17:56:48 +0530

    added components

---
+++ app/components/Providers.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { ImageKitProvider } from "@imagekit/next";
+import { SessionProvider } from "next-auth/react";
+import { NotificationProvider } from "./Notification";
+
+const urlEndPoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <SessionProvider refetchInterval={5 * 60}>
+            <ImageKitProvider urlEndpoint={urlEndPoint}>
+                <NotificationProvider>
+                    {children}
+                </NotificationProvider>
+            </ImageKitProvider>
+        </SessionProvider>
+    );
+}
