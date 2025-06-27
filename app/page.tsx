39e464d352e75993c9d415200cba95ec223092be
SOUNAK GHOSH <sounak2309@gmail.com>
2025-06-27 09:44:13 +0530

    added ui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useNotification } from "./components/Notification";
+import { useNotification } from "../components/Notification";
 
 export default function Home() {
     const { showNotification } = useNotification();
--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import VideoUploadForm from "../components/VideoUploadForm";
+import VideoUploadForm from "../../components/VideoUploadForm";
 
 export default function VideoUploadPage() {
     return (
